21ae354193f8ddc439ccae4a3622d7fa3c78e6e9
Salihan04 <salihan04@gmail.com>
2015-09-15 18:01:10 +0800

    Add function to read file

---
+++ MyMeshViewer/Main.cpp
@@ -0,0 +1,38 @@
+#include <iostream>
+#include <fstream>
+#include <string>
+
+using namespace std;
+
+//Function prototypes
+void parseFile(char* fileName);
+
+int main(int argc, char **argv)
+{
+	parseFile("dummy.txt");
+
+	system("pause");
+
+	return 0;
+}
+
+void parseFile(char* fileName)
+{
+	char str[255];
+	ifstream infile;
+	infile.open(fileName);
+
+	if(!infile.is_open())
+	{
+		cout << "Cannot open dummy.txt" << endl;
+	}
+	
+	while (infile)
+	{
+		infile.getline(str, 255);
+		if (infile)
+			cout << str << endl;
+	}
+
+	infile.close();
+}
